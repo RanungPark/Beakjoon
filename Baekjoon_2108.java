793bda0610248c2cdc7027f91ff90b3538c88982
RanungPark <ckehfqkr302@naver.com>
2022-11-01 16:22:04 +0900

    백준 2108 0%

---
+++ Baekjoon_2108.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Baekjoon_2108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int Average = scanner.nextInt();
+
+//        for(int i=0; i<Average; i++){
+//    }
+
+
+    }
+}
